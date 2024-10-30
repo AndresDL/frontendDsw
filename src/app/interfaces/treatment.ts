fde634d7f3a00a7db4aee13f095f7fd694c8463b
Luca Conti <lucaconti5@outlook.com>
2024-10-30 20:09:23 -0300

    CRUD Treatment

---
+++ src/app/interfaces/treatment.ts
@@ -0,0 +1,5 @@
+export class Treatment {
+    id?: number = 0;
+    name: string = "";
+    price: number = 0;
+}
--- src/app/treatment.service.ts
+++ src/app/treatment.service.ts
@@ -1,22 +1,43 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Treatment } from './interfaces/treatment.js';
+import { response } from 'express';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TreatmentService {
 
-  readonly baseUrl = 'http://localhost:4200/api/'
+  private baseUrl = 'http://localhost:3000/api/treatments/';
   
   constructor(private http: HttpClient){}
   
-  getTreatments(){
-    
-    const url = this.baseUrl + 'treatment';
-    
-    return this.http.get<any>(url);
-
+  getTreatments(): Observable<Treatment[]>{
+    return this.http.get<any>(this.baseUrl).pipe(map(response => {
+      return response.data;
+    }));
   }
 
+  getTreatment(id: number): Observable<Treatment> {
+    const url = `${this.baseUrl}${id}`;
+    return this.http.get<any>(url).pipe(map(response => {
+      return response.data[0];
+    }));
+  }
 
+  addTreatment(Treatment: Treatment): Observable<Treatment> {
+    return this.http.post<Treatment>(this.baseUrl, Treatment);
+  }
+
+  updateTreatment(Treatment: Treatment): Observable<void> {
+    const url = `${this.baseUrl}${Treatment.id}`;
+    return this.http.put<void>(url, Treatment);
+  }
+
+  deleteTreatment(id: number): Observable<void>{
+    const url = `${this.baseUrl}${id}`;
+    return this.http.delete<void>(url);
+  }
 }
